328964ed68d95562babb514769fe37ecff94de2e
Badal Chand <badalchand636@gmail.com>
2026-07-14 10:40:50 +0545

    Make some changes in Achievements page

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,7 +1,6 @@
 import "./globals.css";
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
-
 import { Poppins, Inter } from "next/font/google";
 
 const poppins = Poppins({
@@ -27,13 +26,9 @@ export default function RootLayout({
       className={`${poppins.variable} ${inter.variable}`}
     >
       <body>
-
         <Navbar />
-
         <main>{children}</main>
-
         <Footer />
-
       </body>
     </html>
   );
