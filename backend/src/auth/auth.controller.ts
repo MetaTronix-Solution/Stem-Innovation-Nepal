e869adacefeab5a031e5cb3f74ac7bcae962c503
Badal Chand <badalchand636@gmail.com>
2026-07-17 11:52:47 +0545

    Completed the full backend of the admin login

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
+import type { Response } from 'express';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+
+
+
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(
+        private readonly authService: AuthService,
+    ) {}
+
+
+    @Post("login")
+    login(
+        @Body() loginDto: LoginDto,
+        @Res({passthrough: true}) response: Response,
+    ) {
+        return this.authService.login(loginDto, response)
+    }
+}
--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,8 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Admin, AdminSchema } from 'src/admin/schemas/admin.schema';
+import { JwtModule } from '@nestjs/jwt';
+import {  ConfigService } from '@nestjs/config';
 
 @Module({
+
+  imports:[
+    MongooseModule.forFeature([
+      {
+        name: Admin.name,
+        schema: AdminSchema,
+      },
+    ]),
+
+    JwtModule.registerAsync({
+  inject: [ConfigService],
+  useFactory: (configService: ConfigService) => ({
+    secret: configService.get<string>('JWT_SECRET'),
+    signOptions: {
+      expiresIn: '7d',
+    },
+  }),
+}),
+  ],
+
   controllers: [AuthController],
   providers: [AuthService]
 })
