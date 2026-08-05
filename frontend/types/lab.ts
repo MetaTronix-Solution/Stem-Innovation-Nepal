8bb6f341deead7b72e0c9a981dc6efbff91ea57b
Badal Chand <badalchand636@gmail.com>
2026-08-05 13:59:50 +0545

    Frontedn

--- frontend/types/lab.ts
+++ frontend/types/lab.ts
@@ -1,13 +1,21 @@
-export interface LabItem {
+export interface LabCategory {
   _id: string;
-  title: string;
+  name: string;
 }
 
-export interface Lab {
+export interface LabItem {
   _id: string;
   title: string;
   description: string;
   image: string;
   price: number;
+  specification: string;
+  quantity: number;
+  category: string | LabCategory;
+}
+
+export interface Lab {
+  _id: string;
+  title: string;
   labItems: LabItem[];
 }
