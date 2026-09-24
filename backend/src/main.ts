4e36125038cf564d1d7d0b22b6922ffc016b4bda
Badal Chand <badalchand636@gmail.com>
2026-09-24 22:01:23 +0545

    Working on the lab and lab items

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -14,8 +14,8 @@ async function bootstrap() {
 
   app.enableCors({
   origin: [
-    "http://localhost:3000",
-    "https://stem-innovation-nepal-wecr.vercel.app",
+    "http://localhost:3000"
+
   ],
   credentials: true,
 });
